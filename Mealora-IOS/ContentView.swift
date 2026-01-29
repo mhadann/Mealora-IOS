7e6b03c1ed2754bd1ee385c0d7ecb478785a0b35
Mahamed Adan <adanm6790@gmail.com>
2026-01-29 12:54:12 +0100

    Initial Commit

---
+++ Mealora-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mealora-IOS
+//
+//  Created by Mahamed Adan on 2026-01-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mealora-IOS/Mealora_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mealora_IOSApp.swift
+//  Mealora-IOS
+//
+//  Created by Mahamed Adan on 2026-01-29.
+//
+
+import SwiftUI
+
+@main
+struct Mealora_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
